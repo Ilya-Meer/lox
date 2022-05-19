1b5eb1b2c238714ce590c7b3720028266995774a
Ilya Meerovich <ilya.meerov@gmail.com>
2022-05-18 18:20:46 -0700

    Initial commit

---
+++ com/craftinginterpreters/lox/Scanner.java
@@ -0,0 +1,23 @@
+package com.craftinginterpreters.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.craftinginterpreters.lox.TokenType.*;
+
+public class Scanner {
+  private final String source;
+
+  private final List<Token> tokens = new ArrayList<>();
+
+  Scanner(String source) {
+    this.source = source;
+  }
+
+  public List<Token> scanTokens() {
+
+
+  }
+}
