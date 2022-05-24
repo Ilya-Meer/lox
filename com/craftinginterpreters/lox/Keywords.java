7405483d44328434b30bb4ff994e60220309c6a9
Ilya Meerovich <ilya.meerov@gmail.com>
2022-05-23 21:14:13 -0700

    Add keywords

---
+++ com/craftinginterpreters/lox/Keywords.java
@@ -0,0 +1,30 @@
+package com.craftinginterpreters.lox;
+
+import java.util.HashMap;
+
+public class Keywords {
+    public static final HashMap<String, TokenType> keywords = new HashMap<>();
+
+    static {
+        keywords.put("and",    TokenType.AND);
+        keywords.put("class",  TokenType.CLASS);
+        keywords.put("else",   TokenType.ELSE);
+        keywords.put("false",  TokenType.FALSE);
+        keywords.put("for",    TokenType.FOR);
+        keywords.put("fun",    TokenType.FUN);
+        keywords.put("if",     TokenType.IF);
+        keywords.put("nil",    TokenType.NIL);
+        keywords.put("or",     TokenType.OR);
+        keywords.put("print",  TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super",  TokenType.SUPER);
+        keywords.put("this",   TokenType.THIS);
+        keywords.put("true",   TokenType.TRUE);
+        keywords.put("var",    TokenType.VAR);
+        keywords.put("while",  TokenType.WHILE);
+    }
+
+    public static TokenType getKeyword(String text) {
+        return keywords.get(text);
+    }
+}
